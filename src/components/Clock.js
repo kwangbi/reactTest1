c4fac7fac822c88f6af98e6b758a9418ae8b563c
kwangbi <kwangbi@naver.com>
2021-08-20 14:32:07 +0900

    commit

---
+++ src/components/Clock.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Clock extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(
+            () => this.tick(), 1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState(
+            { date: new Date() }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>현재 시각은 ??</h2>
+                <div>
+                    {this.state.date.toLocaleTimeString()}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Clock;
